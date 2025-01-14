7f27557fcdacf0f4ae17e6e9cb65e8b0a99db4ae
Dustin Stacy <dustin.stacy@outlook.com>
2025-01-13 21:04:41 -0600

    add exponential functions

--- contracts/Boncurs.sol
+++ contracts/Boncurs.sol
@@ -1,4 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-library Boncurs {}
+import {BancorFormula} from "./utils/BancorFormula.sol";
+
+abstract contract Boncurs {
+    BancorFormula public bancorFormula;
+
+    function getExponentialPurchaseReturn(
+        uint256 supply,
+        uint256 reserveBalance,
+        uint32 reserveRatio,
+        uint256 depositAmount
+    ) internal view returns (uint256 purchaseReturn) {
+        return bancorFormula.calculatePurchaseReturn(supply, reserveBalance, reserveRatio, depositAmount);
+    }
+
+    function getExponentialSaleReturn(uint256 supply, uint256 reserveBalance, uint32 reserveRatio, uint256 sellAmount)
+        internal
+        view
+        returns (uint256 saleReturn)
+    {
+        return bancorFormula.calculateSaleReturn(supply, reserveBalance, reserveRatio, sellAmount);
+    }
+}
