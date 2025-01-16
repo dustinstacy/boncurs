3c733021a985b87909bf701e8a36c4fbefa44cd0
Dustin Stacy <dustin.stacy@outlook.com>
2025-01-16 14:32:54 -0600

    Update Boncurs.sol

--- src/contracts/Boncurs.sol
+++ src/contracts/Boncurs.sol
@@ -2,23 +2,44 @@
 pragma solidity ^0.8.28;
 
 import {BancorFormula} from "./utils/BancorFormula.sol";
+import {LinearFormula} from "./utils/LinearFormula.sol";
 
-abstract contract Boncurs is BancorFormula {
+abstract contract Boncurs is BancorFormula, LinearFormula {
     function _getBancorFormulaPurchaseReturn(
-        uint256 supply,
-        uint256 reserveBalance,
-        uint32 reserveRatio,
-        uint256 depositAmount
+        uint256 _supply,
+        uint256 _reserveBalance,
+        uint32 _reserveRatio,
+        uint256 _depositAmount
     ) internal view returns (uint256 purchaseReturn) {
-        return _calculateBancorFormulaPurchaseReturn(supply, reserveBalance, reserveRatio, depositAmount);
+        return _calculateBancorFormulaPurchaseReturn(_supply, _reserveBalance, _reserveRatio, _depositAmount);
     }
 
     function _getBancorFormulaSaleReturn(
-        uint256 supply,
-        uint256 reserveBalance,
-        uint32 reserveRatio,
-        uint256 sellAmount
+        uint256 _supply,
+        uint256 _reserveBalance,
+        uint32 _reserveRatio,
+        uint256 _sellAmount
     ) internal view returns (uint256 saleReturn) {
-        return _calculateBancorFormulaSaleReturn(supply, reserveBalance, reserveRatio, sellAmount);
+        return _calculateBancorFormulaSaleReturn(_supply, _reserveBalance, _reserveRatio, _sellAmount);
+    }
+
+    function _getLinearFormulaPurchaseReturn(
+        uint256 _supply,
+        uint256 _reserveBalance,
+        uint256 _initialPrice,
+        uint32 _scalingFactor,
+        uint256 _depositAmount
+    ) internal pure returns (uint256 purchaseReturn) {
+        return _calculateLinearPurchaseReturn(_supply, _reserveBalance, _initialPrice, _scalingFactor, _depositAmount);
+    }
+
+    function _getLinearFormulaSaleReturn(
+        uint256 _supply,
+        uint256 _reserveBalance,
+        uint256 _initialPrice,
+        uint32 _scalingFactor,
+        uint256 _depositAmount
+    ) internal pure returns (uint256 saleReturn) {
+        return _calculateLinearSaleReturn(_supply, _reserveBalance, _initialPrice, _scalingFactor, _depositAmount);
     }
 }
