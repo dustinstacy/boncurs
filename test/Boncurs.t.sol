4bcc889bb313a970e5f50002d58a876b24b4b002
Dustin Stacy <dustin.stacy@outlook.com>
2025-01-27 12:39:42 -0600

    remove unnecessary files

--- test/Boncurs.t.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-contract BoncursTest {
-    function test() public pure returns (string memory) {
-        return "Hello, World!";
-    }
-}
