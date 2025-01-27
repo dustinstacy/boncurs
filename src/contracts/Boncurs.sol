f31ed96da22492c60e630297ec1fe911a8542a41
Dustin Stacy <dustin.stacy@outlook.com>
2025-01-27 12:42:14 -0600

    update Boncurs.sol'

--- src/contracts/Boncurs.sol
+++ src/contracts/Boncurs.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.28;
 
 import {BancorFormula} from "./utils/BancorFormula.sol";
-import {LinearFormula} from "./utils/LinearFormula.sol";
-import {LinearFormulaV2} from "./utils/LinearFormulaV2.sol";
+import {LinFormula} from "./utils/LinFormula.sol";
 
-abstract contract Boncurs is BancorFormula, LinearFormula, LinearFormulaV2 {
-    function _getBancorFormulaPurchaseReturn(
+abstract contract Boncurs is BancorFormula, LinFormula {
+    function _getExpPurchaseReturn(
         uint256 _supply,
         uint256 _reserveBalance,
         uint32 _reserveRatio,
@@ -15,52 +14,31 @@ abstract contract Boncurs is BancorFormula, LinearFormula, LinearFormulaV2 {
         return _calculateBancorFormulaPurchaseReturn(_supply, _reserveBalance, _reserveRatio, _depositAmount);
     }
 
-    function _getBancorFormulaSaleReturn(
-        uint256 _supply,
-        uint256 _reserveBalance,
-        uint32 _reserveRatio,
-        uint256 _sellAmount
-    ) internal view returns (uint256 saleReturn) {
+    function _getExpSaleReturn(uint256 _supply, uint256 _reserveBalance, uint32 _reserveRatio, uint256 _sellAmount)
+        internal
+        view
+        returns (uint256 saleReturn)
+    {
         return _calculateBancorFormulaSaleReturn(_supply, _reserveBalance, _reserveRatio, _sellAmount);
     }
 
-    function _getLinearFormulaPurchaseReturn(
+    function _getLinPurchaseReturn(
         uint256 _supply,
         uint256 _reserveBalance,
         uint256 _initialPrice,
         uint32 _scalingFactor,
         uint256 _depositAmount
     ) internal pure returns (uint256 purchaseReturn) {
-        return _calculateLinearPurchaseReturn(_supply, _reserveBalance, _initialPrice, _scalingFactor, _depositAmount);
+        return _calculateLinPurchaseReturn(_supply, _reserveBalance, _initialPrice, _scalingFactor, _depositAmount);
     }
 
-    function _getLinearFormulaSaleReturn(
+    function _getLinSaleReturn(
         uint256 _supply,
         uint256 _reserveBalance,
         uint256 _initialPrice,
         uint32 _scalingFactor,
         uint256 _depositAmount
     ) internal pure returns (uint256 saleReturn) {
-        return _calculateLinearSaleReturn(_supply, _reserveBalance, _initialPrice, _scalingFactor, _depositAmount);
-    }
-
-    function _getLinearFormulaV2PurchaseReturn(
-        uint256 _supply,
-        uint256 _reserveBalance,
-        uint256 _initialPrice,
-        uint32 _scalingFactor,
-        uint256 _depositAmount
-    ) internal pure returns (uint256 purchaseReturn) {
-        return _calculateLinearV2PurchaseReturn(_supply, _reserveBalance, _initialPrice, _scalingFactor, _depositAmount);
-    }
-
-    function _getLinearFormulaV2SaleReturn(
-        uint256 _supply,
-        uint256 _reserveBalance,
-        uint256 _initialPrice,
-        uint32 _scalingFactor,
-        uint256 _depositAmount
-    ) internal pure returns (uint256 saleReturn) {
-        return _calculateLinearV2SaleReturn(_supply, _reserveBalance, _initialPrice, _scalingFactor, _depositAmount);
+        return _calculateLinSaleReturn(_supply, _reserveBalance, _initialPrice, _scalingFactor, _depositAmount);
     }
 }
